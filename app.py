4bb9f82d26cb1bce3d86acae247b34869120604c
Elaine Wu <63991329+Elaine-Wu99@users.noreply.github.com>
2021-04-16 09:30:30 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-FA = "https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, FA])
-
-server = app.server
-app.config.suppress_callback_exceptions = True
