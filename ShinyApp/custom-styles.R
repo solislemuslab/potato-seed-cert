012606d270c3de286894853c68d87cc4f65a0d07
WXYS1209 <xwang2684@wisc.edu>
2023-10-30 22:01:27 -0500

    update custom styles

--- ShinyApp/custom-styles.R
+++ ShinyApp/custom-styles.R
@@ -1,3 +1,4 @@
+# Styles of bars
 custom_styles <- 
   tags$head(
     tags$style(HTML("
@@ -17,29 +18,8 @@ custom_styles <-
                   "))
   )
 
-
-custom_styles_text <-
-  tags$div(
-    style = "padding: 20px;",
-    tags$style(type = 'text/css', "
-                            .custom-h1 {
-                              font-size: 28px;
-                              font-weight: bold;
-                              margin-bottom: 20px;
-                            }
-                            .custom-h2 {
-                              font-size: 24px;
-                              font-weight: bold;
-                              margin-top: 30px;
-                              margin-bottom: 10px;
-                            }
-                            .custom-p {
-                              font-size: 16px;
-                              line-height: 1.5;
-                            }")
-  )
-
-custom_styles_code = 
+# Styles of code
+custom_styles_code <- 
   tags$style(HTML("
     code {
       background-color: white;
